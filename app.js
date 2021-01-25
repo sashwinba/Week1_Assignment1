1bcb2db9f3163fbc86bf09d5e77adc0da7349293
sashwinba <ashwinba@zeta.tech>
2021-01-25 20:04:55 +0530

    Adding Netflix-Clone

---
+++ app.js
@@ -0,0 +1,263 @@
+const data = [{
+        "title": "House of Cards",
+        "year": "2013–",
+        "description": "A Congressman works with his equally conniving wife to exact revenge on the people who betrayed him",
+        "poster": "hoc.jpg",
+        "imdbID": "tt1856010",
+        "trailer": "NTzycsqxYJ0"
+    }, {
+        "title": "Daredevil",
+        "year": "2015–",
+        "description": "A blind lawyer with his other senses superhumanly enhanced fights crime as a costumed superhero.",
+        "poster": "dd.jpg",
+        "imdbID": "tt3322312",
+        "trailer": "m5_A0Wx0jU4"
+    }, {
+        "title": "Orange Is the New Black",
+        "year": "2013–",
+        "description": "The story of Piper Chapman, a woman in her thirties who is sentenced to fifteen months in prison after being convicted of a decade-old crime of transporting money for her drug-dealing girlfriend.",
+        "poster": "oitnb.jpg",
+        "imdbID": "tt2372162",
+        "trailer": "th8WT_pxGqg"
+    }, {
+        "title": "Arrested Development",
+        "year": "2003–",
+        "description": "Level-headed son Michael Bluth takes over family affairs after his father is imprisoned. But the rest of his spoiled, dysfunctional family are making his job unbearable.",
+        "poster": "ad.jpg",
+        "imdbID": "tt0367279",
+        "trailer": "vzVhPCMAxWQ"
+    }, {
+        "title": "Unbreakable Kimmy Schmidt",
+        "year": "2015–",
+        "description": "A woman is rescued from a doomsday cult and starts life over again in New York City.",
+        "poster": "uks.jpg",
+        "imdbID": "tt3339966",
+        "trailer": "ZBJHP2B4bjc"
+    }, {
+        "title": "Marco Polo",
+        "year": "2014–",
+        "description": "In a world replete with greed, betrayal, sexual intrigue and rivalry, \"Marco Polo\" is based on the famed explorer's adventures in Kublai Khan's court in 13th century China.",
+        "poster": "mp.jpg",
+        "imdbID": "tt2189461",
+        "trailer": "CeUhXYmGKU4"
+    }, {
+        "title": "Bloodline",
+        "year": "2015–",
+        "description": "A family of adult siblings find that their secrets and scars are revealed when their black sheep of a brother returns home.",
+        "poster": "bl.jpg",
+        "imdbID": "tt3520702",
+        "trailer": "tRnS8FkcXNk"
+    }, {
+        "title": "BoJack Horseman",
+        "year": "2014–",
+        "description": "Meet the most beloved sitcom horse of the '90s - 20 years later. BoJack Horseman was the star of the hit TV show \"Horsin' Around,\" but today he's washed up, living in Hollywood, complaining about everything, and wearing colorful sweaters.",
+        "poster": "bh.jpg",
+        "imdbID": "tt3398228",
+        "trailer": "5sGsBoX6vUo"
+    }, {
+        "title": "Hemlock Grove",
+        "year": "2013–",
+        "description": "A teenage girl is brutally murdered, sparking a hunt for her killer. But in a town where everyone hides a secret, will they find the monster among them?",
+        "poster": "hg.jpg",
+        "imdbID": "tt2309295",
+        "trailer": "rlZUsPcChgI"
+    }, {
+        "title": "The Killing",
+        "year": "2011–",
+        "description": "A police investigation, the saga of a grieving family, and a Seattle mayoral campaign all interlock after the body of 17-year-old Rosie Larsen is found in the trunk of a submerged car.",
+        "poster": "tk.jpg",
+        "imdbID": "tt1637727",
+        "trailer": "OkbfvPeH3G0"
+    }, {
+        "title": "Love",
+        "year": "2016–",
+        "description": "A program that follows a couple who must navigate the exhilarations and humiliations of intimacy, commitment and other things they were hoping to avoid.",
+        "poster": "l.jpg",
+        "imdbID": "tt4061080",
+        "trailer": "Ym3LoSj9Xj8"
+    }, {
+        "title": "Master of None",
+        "year": "2015–",
+        "description": "The personal and professional life of Dev, a 30-year-old actor in New York.",
+        "poster": "mon.jpg",
+        "imdbID": "tt4635276",
+        "trailer": "6bFvb3WKISk"
+    }, {
+        "title": "Flaked",
+        "year": "2016–",
+        "description": "Will Arnett will star as a self-help guru who needs a decent amount of help himself.",
+        "poster": "f.jpg",
+        "imdbID": "tt4973548",
+        "trailer": "iKOpvm7BcOo"
+    }, {
+        "title": "Jessica Jones",
+        "year": "2015–",
+        "description": "A former superhero decides to reboot her life by becoming a private investigator.",
+        "poster": "jj.jpg",
+        "imdbID": "tt2357547",
+        "trailer": "nWHUjuJ8zxE"
+    }, {
+        "title": "Narcos",
+        "year": "2015–",
+        "description": "A chronicled look at the criminal exploits of Colombian drug lord Pablo Escobar.",
+        "poster": "n.jpg",
+        "imdbID": "tt2707408",
+        "trailer": "U7elNhHwgBU"
+    }, {
+        "title": "Wet Hot American Summer: First Day of Camp",
+        "year": "2015–",
+        "description": "Follow the counselors and campers on their first day at Camp Firewood in the summer of 1981.",
+        "poster": "whas.jpg",
+        "imdbID": "tt4341500",
+        "trailer": "PLlMTn_Jzok"
+    }, {
+        "title": "Club de Cuervos",
+        "year": "2015–",
+        "description": "When the patriarch of a prominent family dies, his heirs battle to determine who will gain control of his beloved soccer team: The Cuervos of Nuevo Toledo.",
+        "poster": "cdc.jpg",
+        "imdbID": "tt4680240",
+        "trailer": "TvUVBVRlCV8"
+    }, {
+        "title": "Making a Murderer",
+        "year": "2015–",
+        "description": "Filmed over a 10-year period, Making a Murderer is an unprecedented real-life thriller about Steven Avery, a DNA exoneree who, while in the midst of exposing corruption in local law enforcement, finds himself the prime suspect in a grisly new crime.",
+        "poster": "mam.jpg",
+        "imdbID": "tt5189670",
+        "trailer": "qxgbdYaR_KQ"
+    }, {
+        "title": "Sense8",
+        "year": "2015–",
+        "description": "A group of people around the world are suddenly linked mentally, and must find a way to survive being hunted by those who see them as a threat to the world's order.",
+        "poster": "s8.jpg",
+        "imdbID": "tt2431438",
+        "trailer": "riLgCIvE9aU"
+    }, {
+        "title": "Fuller House",
+        "year": "2016–",
+        "description": "D.J. Tanner-Fuller is a widow and mother of three. Things become too much to handle, so she asks for help from her sister Stephanie and her best friend Kimmy.",
+        "poster": "fh.jpg",
+        "imdbID": "tt3986586",
+        "trailer": "CXuGLswn2l0"
+    }, {
+        "title": "w/ Bob & David",
+        "year": "2016–",
+        "description": "Bob and David emerge from the time machine they entered sixteen years ago and are reunited with the cast of Mr. Show.",
+        "poster": "wbd.jpg",
+        "imdbID": "tt4574708",
+        "trailer": "QyZ9l8anS9I"
+    }];
+
+const input = document.querySelector('input');
+const section = document.querySelector("section");
+const contentSiz = 125;
+const titleSiz = 11;
+function generatePage(title){
+    window.open(`MoviePage.html?${title}`);
+}
+
+
+function truncateContent(description){
+    if(description.length <= contentSiz){
+        return description;
+    }
+    let truncatedDescription = description.substr(0,contentSiz-3) 
+        + `<a onclick='generatePage(this.parentNode.getAttribute("title"))' title="Click to know more">...</a>` ;
+    return truncatedDescription;
+}
+
+function truncateTitle(title){
+    if(title.length <= titleSiz){
+        return title;
+    }
+
+    let truncatedTitle = title.substr(0,titleSiz-2) + "..";
+    return truncatedTitle;
+}
+
+input.addEventListener('input', function () {
+    let inputTitle = input.value.toUpperCase(); 
+    let inputTitleLength = inputTitle.length;
+
+    let movies = section.querySelectorAll(".movie");
+    for(let movie of movies){
+        let title = movie.getAttribute("title");
+        if(title.length >= inputTitleLength && title.substr(0,inputTitleLength)===inputTitle){
+            movie.style.display = "flex";
+        }
+        else{
+            movie.style.display = "none";
+        }
+    }
+})
+
+
+function createContent(movie){
+
+    let h1Link = document.createElement("a");
+    h1Link.setAttribute("title", movie.title);
+    h1Link.setAttribute("onclick", `generatePage(this.getAttribute("title"))`);
+    h1Link.setAttribute("class" , "titlewrapper");
+    
+    let h1 = document.createElement("h1");
+    h1.textContent = truncateTitle(movie.title);
+    h1.setAttribute("class" , "compressedTitle");
+
+    let h1Expanded = document.createElement("h1");
+    h1Expanded.textContent = movie.title;
+    h1Expanded.setAttribute("class" , "expandedTitle");
+    h1Link.appendChild(h1);
+    h1Link.appendChild(h1Expanded);
+
+    let img = document.createElement("img");
+    img.setAttribute("src", "img/imdb.png")
+    img.setAttribute("class" , "imdb");
+    img.setAttribute("alt" , "imbd");
+    let a = document.createElement("a");
+    a.setAttribute("href" , `https://www.imdb.com/title/${movie.imdbID}`);
+    a.setAttribute("target" , "_blank");
+    a.appendChild(img);
+
+    let h2 = document.createElement("h2");
+    h2.textContent = `(${movie.year})`;
+
+    let p = document.createElement("p");
+    p.innerHTML = truncateContent(movie.description,movie.title);
+    p.setAttribute("title", movie.title);
+
+    let content = document.createElement("div");
+    content.setAttribute("class" , "content");
+
+    content.appendChild(h1Link);
+    content.appendChild(a);
+    content.appendChild(h2);
+    content.appendChild(p);
+
+    return content;
+
+}
+function createImg(movie){
+    let img = document.createElement("img");
+    img.setAttribute("src" , `img/posters/${movie.poster}`);
+    img.setAttribute("class" , "poster");
+    img.setAttribute("alt" , `${movie.title}`);
+    img.setAttribute("title", movie.title);
+    img.setAttribute("onclick", `generatePage(this.getAttribute("title"))`);
+    return img;
+}
+
+
+function initialize(){
+    for(const movie of data){
+        movie.title = movie.title.toUpperCase();
+    }
+    for(const movie of data){
+        let content = createContent(movie);
+        let img = createImg(movie);
+        let divMovie = document.createElement("div");
+        divMovie.setAttribute("class" , "movie");
+        divMovie.setAttribute("title" , movie.title);
+        divMovie.appendChild(img);
+        divMovie.appendChild(content);
+        section.prepend(divMovie);
+    }   
+}
